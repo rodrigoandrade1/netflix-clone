99acad75b8b6eb2205dec037201a855dc7c9d4a8
Rodrigo <rodriigosantos11@gmail.com>
2022-09-21 04:56:56 -0300

    publicpath update

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, "./docs")
+  outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: "netflix-clone"
 })
